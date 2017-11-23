b8a07d9c7a8b6e29930d52ac16aa33a22d01582c
Deomitrus <github@deomitrus.com>
2017-11-23 15:35:38 -0500

    i give up - gonna have a dynamic loader. can optimize with codemod if big deal

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const platforms = new Map([
+  ['darwin', 'mac'],
+  ['win32', 'win'],
+  ['linux', 'linux'],
+])
+
+const suffix = platform.get(process.platform)
+if (!suffix) throw new Error(`Unsupported platform ${process.platform}`)
+exports.default = require('@veonim/neovim-${suffix}').default
