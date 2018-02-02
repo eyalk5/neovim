8239bff0518172f572d4080c050db73416ea8b40
Deomitrus <github@deomitrus.com>
2018-02-02 13:46:18 -0500

    also export vimpath

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,4 @@ import { SpawnOptions, ChildProcess } from 'child_process'
 declare const _default: (args?: string[], options?: SpawnOptions) => ChildProcess
 export default _default
 export const vimruntime: string
+export const vimpath: string
--- main.js
+++ main.js
@@ -10,3 +10,4 @@ const suffix = platforms.get(process.platform)
 if (!suffix) throw new Error(`Unsupported platform ${process.platform}`)
 exports.default = require(`@veonim/neovim-${suffix}`).default
 exports.vimruntime = require(`@veonim/neovim-${suffix}`).vimruntime
+exports.vimpath = require(`@veonim/neovim-${suffix}`).vimpath
