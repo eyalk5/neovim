99e1ef03be3eea0cc44f5c4c36efc161f6cf76a7
breja <gh@breja.io>
2018-03-02 21:16:42 -0500

    include runtime files for both windows and linux

--- linux/runtime/CMakeFiles/helptags.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/helptags"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/helptags.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- linux/runtime/CMakeFiles/vimball-tags.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vimball-tags"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vimball-tags.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
